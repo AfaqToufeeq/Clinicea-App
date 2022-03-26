b7011977c73b26cb8c4b750411897dbf79b15f86
AfaqToufeeq07 <afaqtoufeeq7@gmail.com>
2022-03-26 23:19:50 +0500

    made new project

---
+++ app/src/main/java/com/patient/clinicea/Registration/loginActivity.java
@@ -0,0 +1,16 @@
+package com.patient.clinicea.Registration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.patient.clinicea.R;
+
+public class loginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
