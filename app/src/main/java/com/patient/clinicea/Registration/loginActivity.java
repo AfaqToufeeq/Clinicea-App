295ea5c5c75475397f493ef7a272601b8266bad1
AfaqToufeeq07 <afaqtoufeeq7@gmail.com>
2022-03-27 23:24:25 +0500

    home dashboard

--- app/src/main/java/com/patient/clinicea/Registration/loginActivity.java
+++ app/src/main/java/com/patient/clinicea/Registration/loginActivity.java
@@ -8,16 +8,33 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.patient.clinicea.Dashboard.dashboardActivity;
 import com.patient.clinicea.R;
 
 public class loginActivity extends AppCompatActivity {
+
+    EditText emailAddress,pass;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        emailAddress=findViewById(R.id.emailAddressTV);
+        pass=findViewById(R.id.passTV);
+        login=findViewById(R.id.loginButton);
+
         changeStatusBarColor();
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), dashboardActivity.class));
+            }
+        });
     }
 
 
